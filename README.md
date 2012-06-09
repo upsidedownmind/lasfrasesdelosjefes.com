8028718322b343ce3d01b07a8603ac45ae58c3fa
Valentin Starck <valentin.starck@gmail.com>
2012-06-08 23:47:12 -0300

    First commit
    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Las frases de los jefes
+=======================
+lasfrasesdelosjefes.com
+
+Porque queremos preservar esos mágicos momentos.
---
+++ data.js
@@ -0,0 +1,3 @@
+var data = [
+	'No pagamos horas extras porque para eso hay que hacer micro-managment de los horarios, y eso no me gusta'
+];
