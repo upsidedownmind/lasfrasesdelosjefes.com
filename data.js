e30523d2730fba1fb84ad61b62d1a3623cc07f6d
upside-down mind <francisco.affonso@gmail.com>
2012-06-09 00:10:27 -0300

    un par de frases top

--- data.js
+++ data.js
@@ -1,3 +1,7 @@
 var data = [
-	'No pagamos horas extras porque para eso hay que hacer micro-managment de los horarios, y eso no me gusta'
+	'No pagamos horas extras porque para eso hay que hacer micro-managment de los horarios, y eso no me gusta',
+	'Naa.. A mi hay veces que la justicia no me conviene',
+	'Salgo un rato, después vuelvo y lo resolvemos juntos',
+	'Pero eso... no va a funcionar .. (..depues de funcionar 3 meses en producción)',
+	'Debe ser una boludes, ¿lo hacemos para mañana?',
 ];
